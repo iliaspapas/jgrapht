e1cb53f70d8dbf1e1dd01ec35db53a1b1ac43518
iliaspapas <lioxperia54@gmail.com>
2020-09-10 00:28:53 +0300

    ptuxiaki

--- jgrapht-core/src/main/java/org/jgrapht/alg/drawing/KKLayoutAlgorithm.java
+++ jgrapht-core/src/main/java/org/jgrapht/alg/drawing/KKLayoutAlgorithm.java
@@ -12,10 +12,10 @@ import java.util.Random;
 
 /*This algorithm is based on the algorithm kamada and kawai build for graph drawing.
 
-    This is an algorithm which successfully drawes undirected graphs.The basic idea of the algorithm
+    This is an algorithm which successfully draws undirected graphs.The basic idea of the algorithm
     is that we consider the desirable geometric distance between two vertices in the drawing
     as the graph theoretic distance between them in the corresponding graph . This algorithm
-    considers the edges between two vertices as springs. So the algorithm drawes a graph
+    considers the edges between two vertices as springs. So the algorithm draws a graph
     which is the optimal layout of vertices as the state in which the total spring energy
     of the system is minimal
 
@@ -36,6 +36,11 @@ public class KKLayoutAlgorithm<V, E> extends
 
     /**
      * Create a new layout algorithm
+     * @param sgraph the simple directed graph
+     * @param mlayoutmodel the model
+     * @param K constant
+     * @param epsilon constant
+
      */
     public KKLayoutAlgorithm(SimpleGraph<V, E> sgraph, MapLayoutModel2D<V> mlayoutmodel, double K, double epsilon) {
         this(new Random(), sgraph, mlayoutmodel, K, epsilon);
@@ -43,8 +48,12 @@ public class KKLayoutAlgorithm<V, E> extends
 
     /**
      * Create a new layout algorithm
-     *
-     * @param rng the random number generator
+     *  @param rng the random number generator
+     * @param sgraph the simple directed graph
+     * @param mlayoutmodel the model
+     * @param K constant
+     * @param epsilon constant
+
      */
     public KKLayoutAlgorithm(Random rng, SimpleGraph<V, E> sgraph, MapLayoutModel2D<V> mlayoutmodel, double K, double epsilon) {
         this.K = K;
@@ -58,18 +67,34 @@ public class KKLayoutAlgorithm<V, E> extends
     /**
      * Create a new layout algorithm
      *
-     * @param rng the random number generators
+     * @param rng the random number generator
+     * @param K constant
+     * @param epsilon constant
      */
     public KKLayoutAlgorithm(Random rng, double K, double epsilon) {
         this.rng = Objects.requireNonNull(rng);
         this.K = K;
         this.epsilon = epsilon;
     }
-
+    /**
+     * Execute layout algorithm
+     * @param sgraph the simple directed graph
+     * @param mlayoutmodel the model
+     */
     private void executealgorithm(SimpleGraph<V, E> sgraph, MapLayoutModel2D<V> mlayoutmodel) {
         layout(sgraph, mlayoutmodel);
     }
-
+    /**
+     * Calculate dx & dy
+     *
+     * @param n number of nodes
+     * @param k strength of spring
+     * @param l length of edge
+     * @param maxm node with max Dm
+     * @param E_xm derivative by xm of energy
+     * @param E_ym derivative by ym of energy
+     * @return pair including dx and dy values
+     */
     private Pair<Double, Double> calculateDxDy(int n, double[][] k, double[][] l, int maxm, double[] E_xm, double[] E_ym) {
         double Energy2_xm = 0;
         double Energy2_ym = 0;
@@ -88,12 +113,18 @@ public class KKLayoutAlgorithm<V, E> extends
         double denominator = Energy2_xm * Energy2_ym - Energy2_yx * Energy2_xy;
         Dx = (Energy2_xy * E_ym[maxm] - Energy2_ym * E_xm[maxm]) / denominator;
         Dy = (Energy2_yx * E_xm[maxm] - Energy2_xm * E_ym[maxm]) / denominator;
-
         Pair d;
         d = new Pair(Dx, Dy);
         return d;
     }
-
+    /**
+     * Calculate L
+     *
+     * @param n number of nodes
+     * @param l length of edge
+     * @param L0 length of a side of display square area
+     * @param d shortest paths
+     */
     private void calculateL(int n, double[][] l, double L0, double[][] d) {
         double maxpathweight = 0;
         for (int i = 0; i < n; i++) {
@@ -110,7 +141,16 @@ public class KKLayoutAlgorithm<V, E> extends
             }
         }
     }
-
+    /**
+     * Calculate Exm & Ey
+     *
+     * @param E_xm derivative by xm of energy
+     * @param E_ym derivative by ym of energy
+     * @param l length of edge
+     * @param k strength of spring
+     * @param n number of nodes
+     * @param m node
+     */
     private void calculateExm_Eym(double[] E_xm, double[] E_ym, double[][] l, double[][] k, int n, int m) {
         for (int i = 0; i < n; i++) {
             if (i != m) {
@@ -119,6 +159,16 @@ public class KKLayoutAlgorithm<V, E> extends
             }
         }
     }
+
+    /**
+     * Adjust nodes to the center of display area
+     * @param width the width of display area
+     * @param height the height of display area
+     * @param x the x coordinate of vertices
+     * @param y the y coordinate of vertices
+     * @param graph the graph
+     * @param model the model
+     */
 
     public void adjustForGravity(double width, double height, double[] x, double[] y, Graph<V, E> graph, LayoutModel2D<V> model) {
 
@@ -147,6 +197,15 @@ public class KKLayoutAlgorithm<V, E> extends
         }
     }
 
+    /**
+     * Calculate energy
+     * @param n number of nodes
+     * @param d shortest paths between vertices
+     * @param x x coordinate of vertices
+     * @param y y coordinate of vertices
+     * @return energy
+     */
+
     private double calcEnergy(int n, double[][] d, double[] x, double[] y) {
         double energy = 0;
         for (int i = 0; i < n - 1; i++) {
@@ -166,7 +225,15 @@ public class KKLayoutAlgorithm<V, E> extends
 
     /**
      * Calculates the energy function E as if positions of the specified nodes are exchanged.
+     * @param p source vertex
+     * @param q destination vertex
+     * @param n number of nodes
+     * @param d shortest paths among vertices
+     * @param x x coordinate of vertices
+     * @param y y coordinate of vertices
+     * @return energy
      */
+
     private double calcEnergyIfExchanged(int p, int q, int n, double[][] d, double[] x, double[] y) {
         if (p >= q) {
             throw new RuntimeException("p should be < q");
@@ -197,6 +264,10 @@ public class KKLayoutAlgorithm<V, E> extends
     }
 
     @Override
+    /**
+     * Calculate the optimal positions of vertices in display area using Kamada-Kawai layout algorithm
+     *
+     */
     public void layout(Graph<V, E> graph, LayoutModel2D<V> model) {
 
         // read area
@@ -233,7 +304,7 @@ public class KKLayoutAlgorithm<V, E> extends
         }
         int i, j;
         //veltisth apostash sto graph metaksy korifwn--L
-        // L = normalizationFactor * Math.sqrt(area / n);
+
         FloydWarshallShortestPaths spath = new FloydWarshallShortestPaths(graph);
         double[][] d = new double[n][n];
         i = 0;
@@ -247,11 +318,11 @@ public class KKLayoutAlgorithm<V, E> extends
         }
         double L0;
         if (width < height) {
-            L0 = width; //L0=max(width,height)
+            L0 = width;
         } else {
             L0 = height;
         }
-        //L=L0/maxpath
+
         double[][] l = new double[d.length][d[0].length];
         double[][] k = new double[l.length][l[0].length];
         calculateL(n, l, L0, d);
@@ -312,7 +383,7 @@ public class KKLayoutAlgorithm<V, E> extends
                 E_ym[maxm] = 0;
                 calculateExm_Eym(E_xm, E_ym, l, k, n, maxm);
                 Dm = Math.sqrt(E_xm[maxm] * E_xm[maxm] + E_ym[maxm] * E_ym[maxm]);
-                // max_Dm = Dm;
+
                 if (Dm < epsilon) break;
             }
             adjustForGravity(width, height, x, y, graph, model);
